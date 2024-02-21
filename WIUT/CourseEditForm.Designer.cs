ef3d28933ebf3d797fd0009ba776beabea375c61
00016885 <wiuts11@gmail.com>
2024-02-21 11:06:54 +0500

    Add project files.

---
+++ WIUT/CourseEditForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WIUT
+{
+    partial class CourseEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblName = new Label();
+            tbxName = new TextBox();
+            btnSave = new Button();
+            btnCancel = new Button();
+            SuspendLayout();
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(63, 38);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(39, 15);
+            lblName.TabIndex = 0;
+            lblName.Text = "Name";
+            // 
+            // tbxName
+            // 
+            tbxName.Location = new Point(120, 35);
+            tbxName.Name = "tbxName";
+            tbxName.Size = new Size(116, 23);
+            tbxName.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(63, 81);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 1;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(161, 81);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 2;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // CourseEditForm
+            // 
+            AcceptButton = btnSave;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new Size(310, 148);
+            Controls.Add(btnCancel);
+            Controls.Add(btnSave);
+            Controls.Add(tbxName);
+            Controls.Add(lblName);
+            Name = "CourseEditForm";
+            Text = "CourseEditForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblName;
+        private TextBox tbxName;
+        private Button btnSave;
+        private Button btnCancel;
+    }
+}
